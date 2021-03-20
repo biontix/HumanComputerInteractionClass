545104c2740a8d4fb491f9556d33d7da4a71070d
Cristian Vitali <41077457+biontix@users.noreply.github.com>
2021-03-20 15:03:18 +0100

    Update SQLConnection.java

--- LanternaExercise3/src/Control/SQLConnection.java
+++ LanternaExercise3/src/Control/SQLConnection.java
@@ -1,5 +1,21 @@
 package Control;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class SQLConnection {
 
+	static Connection con = null;
+	public static Connection dbConnector(){
+		try{
+			
+			Class.forName("org.sqlite.JDBC");
+			Connection con = DriverManager.getConnection("jdbc:sqlite:W.sqlite");
+			return con;
+			
+		}catch(Exception e){
+			System.out.println("Connection Error"+e.getMessage());
+		}
+		return con;
+	}
 }
