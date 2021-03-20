8efbeba3bde8dc22c7287a87bda25d3f59cfe728
Cristian Vitali <41077457+biontix@users.noreply.github.com>
2021-03-20 12:28:36 +0100

    Update Customer.java

--- LanternaExercise3/src/Model/Customer.java
+++ LanternaExercise3/src/Model/Customer.java
@@ -1,5 +1,34 @@
 package Model;
 
 public class Customer {
+	
+    private String CLID;
+    private String name;
+    private String surname;
+    
+
+    public String getCLID() {
+        return CLID;
+    }
+
+    public void setCLID(String CLID) {
+        this.CLID = CLID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 
 }
