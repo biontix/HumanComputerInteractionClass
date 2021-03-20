d3a264f53c3742bd86894fd9aaffdf4a0021cb6c
Cristian Vitali <41077457+biontix@users.noreply.github.com>
2021-03-20 12:37:51 +0100

    Update Supplier.java

--- LanternaExercise3/src/Model/Supplier.java
+++ LanternaExercise3/src/Model/Supplier.java
@@ -1,5 +1,21 @@
 package Model;
 
 public class Supplier {
-
+	
+    private String SUID;
+    private String name;
+    
+	public String getSUID() {
+		return SUID;
+	}
+	public void setSUID(String sUID) {
+		SUID = sUID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+    
 }
